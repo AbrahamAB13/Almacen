52cf6db3e9b05f5a7a110696ab765bf0cb433a79
Tesoem <Tesoem@ISC-LAB->
2023-06-23 17:08:01 -0600

    MAAB

---
+++ src/MAAB4s21/MAABP34S21.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p3cmr4s21;
+
+/**
+ *
+ * @author Tesoem
+ */
+public class P3CMR4S21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new JFPrincipal().setVisible(true);
+    }
+    
+}
---
+++ src/informacion/info_nombre.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package informacion;
+
+/**
+ *
+ * @author Tesoem
+ */
+public class info_nombre {
+String nombres;
+String ApellidoP;
+String ApellidoM;
+
+
+    public info_nombre(String nombres, String paterno, String materno) {
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.materno = materno;
+    }
+
+    public info_nombre() {
+    }
+
+    public String getnombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getpaterno() {
+        return paterno;
+    }
+
+    public void setpaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getmaterno() {
+        return materno;
+    }
+
+    public void setmaterno(String materno) {
+        this.materno = materno;
+    }
+
+
+
+}
